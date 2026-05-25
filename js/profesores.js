7d1a53aab6e5e56bf3ca6418ce36a2d6dcb42f04
KeyserDarkRoot <broshmario44@gmail.com>
2026-05-24 23:42:36 -0500

    Iplementacion de interfaz de profesores y funcionalidades con js

---
+++ js/profesores.js
@@ -0,0 +1,267 @@
+const usuario =
+JSON.parse(
+    localStorage.getItem(
+        "usuarioActivo"
+    )
+);
+
+// VALIDAR
+
+if(!usuario){
+
+    window.location.href =
+    "../index.html";
+}
+
+// ELEMENTOS
+
+const bienvenida =
+document.getElementById(
+    "bienvenida"
+);
+
+const avatarImg =
+document.getElementById(
+    "avatarImg"
+);
+
+const tablaTutorias =
+document.getElementById(
+    "tablaTutorias"
+);
+
+// CARDS
+
+const pendientesCount =
+document.getElementById(
+    "pendientesCount"
+);
+
+const aprobadasCount =
+document.getElementById(
+    "aprobadasCount"
+);
+
+const finalizadasCount =
+document.getElementById(
+    "finalizadasCount"
+);
+
+// MOSTRAR DATOS
+
+bienvenida.textContent =
+`Bienvenido, ${usuario.nombre}`;
+
+avatarImg.src =
+usuario.foto;
+
+// LOGOUT
+
+document.getElementById("logout")
+.addEventListener("click", ()=>{
+
+    localStorage.removeItem(
+        "usuarioActivo"
+    );
+
+    window.location.href =
+    "../index.html";
+
+});
+
+// TUTORIAS
+
+let tutorias = [];
+
+// CARGAR
+
+function cargarTutorias(){
+
+    const tutoriasGuardadas =
+
+    localStorage.getItem(
+        "tutorias"
+    );
+
+    if(tutoriasGuardadas){
+
+        tutorias =
+        JSON.parse(
+            tutoriasGuardadas
+        );
+
+    }else{
+
+        tutorias = [];
+
+    }
+
+}
+
+// RENDER
+
+function renderTutorias(){
+
+    tablaTutorias.innerHTML = "";
+
+    // SOLO DEL PROFESOR
+
+    const tutoriasProfesor =
+    tutorias.filter(t =>
+
+        t.profesorId === usuario.id
+    );
+
+    tutoriasProfesor.forEach(tutoria => {
+
+        const tr =
+        document.createElement("tr");
+
+        tr.innerHTML = `
+
+            <td>
+                ${tutoria.estudiante}
+            </td>
+
+            <td>
+                ${tutoria.materia}
+            </td>
+
+            <td>
+                ${tutoria.fecha}
+            </td>
+
+            <td>
+                ${tutoria.hora}
+            </td>
+
+            <td>
+                ${tutoria.estado}
+            </td>
+
+            <td>
+
+                <button
+                    class="aprobar"
+                    onclick="aprobarTutoria(${tutoria.id})"
+                >
+                    Aprobar
+                </button>
+
+                <button
+                    class="rechazar"
+                    onclick="rechazarTutoria(${tutoria.id})"
+                >
+                    Rechazar
+                </button>
+
+            </td>
+        `;
+
+        tablaTutorias.appendChild(tr);
+
+    });
+
+}
+
+// APROBAR
+
+function aprobarTutoria(id){
+
+    const tutoria =
+    tutorias.find(t =>
+
+        t.id === id
+    );
+
+    tutoria.estado =
+    "Aprobada";
+
+    guardarCambios();
+
+}
+
+// RECHAZAR
+
+function rechazarTutoria(id){
+
+    const tutoria =
+    tutorias.find(t =>
+
+        t.id === id
+    );
+
+    tutoria.estado =
+    "Rechazada";
+
+    guardarCambios();
+
+}
+
+// GUARDAR
+
+function guardarCambios(){
+
+    localStorage.setItem(
+        "tutorias",
+        JSON.stringify(tutorias)
+    );
+
+    renderTutorias();
+
+    actualizarCards();
+
+}
+
+// CARDS
+
+function actualizarCards(){
+
+    const tutoriasProfesor =
+    tutorias.filter(t =>
+
+        t.profesorId === usuario.id
+    );
+
+    // PENDIENTES
+
+    const pendientes =
+    tutoriasProfesor.filter(t =>
+
+        t.estado === "Pendiente"
+    ).length;
+
+    // APROBADAS
+
+    const aprobadas =
+    tutoriasProfesor.filter(t =>
+
+        t.estado === "Aprobada"
+    ).length;
+
+    // FINALIZADAS
+
+    const finalizadas =
+    tutoriasProfesor.filter(t =>
+
+        t.estado === "Finalizada"
+    ).length;
+
+    pendientesCount.textContent =
+    pendientes;
+
+    aprobadasCount.textContent =
+    aprobadas;
+
+    finalizadasCount.textContent =
+    finalizadas;
+
+}
+
+// INICIAR
+
+cargarTutorias();
+
+renderTutorias();
+
+actualizarCards();
